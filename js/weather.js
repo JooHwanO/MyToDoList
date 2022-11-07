6fd9d9710a4c2cc86ff4072c7fb745a35197383f
JooHwanO <ghksdh587@naver.com>
2022-11-07 20:42:51 +0900

    css 꾸미기

--- js/weather.js
+++ js/weather.js
@@ -4,6 +4,7 @@ function onGeoOk(position)
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    
     fetch(url).then(response => response.json()).then(data =>{
         const weather= document.querySelector("#weather span:first-child");
         const city= document.querySelector("#weather span:last-child");
