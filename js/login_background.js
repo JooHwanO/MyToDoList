c762113cf61c7f102367f35beef0322c363fc9cf
JooHwanO <ghksdh587@naver.com>
2022-11-09 21:45:00 +0900

    css 연습

--- js/login_background.js
+++ js/login_background.js
@@ -4,9 +4,10 @@
 const login_image=["귀를기울이면.gif","LOVE.gif","LP.gif","man.gif","SIGA2.gif","so.gif"];
 
 const chosenImage = login_image[Math.floor(Math.random() * login_image.length)];
-
-const mainselect=document.querySelector('main'); //메인찾기 (쿼리셀렉터는 하나의 값만 가져옴)
+const mainselect=document.querySelectorAll('div').item(2);
+console.log(mainselect);
 const loginimg = document.createElement("img");
 loginimg.src=`loginimg/${chosenImage}`;
-//document.body.main.appendChild(loginimg); 
-mainselect.appendChild(loginimg); 
+
+mainselect.appendChild(loginimg); //메인찾기 (쿼리셀렉터는 하나의 값만 가져옴)
+
