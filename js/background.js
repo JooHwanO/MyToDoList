2687ab9c459ccb2df6456742bfde4c5ada922bc9
JooHwanO <ghksdh587@naver.com>
2022-11-16 21:29:20 +0900

    main 작업

--- js/background.js
+++ js/background.js
@@ -1,15 +1,13 @@
 
 
-const images=["01.jpg","02.jpg","03.jpg"];
 
+
+
+const images=["COKE.gif"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bdgImage = document.createElement("img");
-bdgImage.src=`img/${chosenImage}`; // 여기까지하면 자바스크립트에만 img가 생성되 존재하므로
+const bdgImage = document.getElementById("main-img");
+const loginimg1 = document.createElement("img");
+loginimg1.src=`img/${chosenImage}`; // 여기까지하면 자바스크립트에만 img가 생성되 존재하므로
                                    //html의 body에 추가해주어야한다 
-
-document.body.appendChild(bdgImage); //body에 추가하는 부분 
-
-
-/*------------------------------------------------------------- */
+bdgImage.appendChild(loginimg1); //body에 추가하는 부분 
 
--- js/login_background.js
+++ js/login_background.js
@@ -5,7 +5,11 @@ const login_image=["귀를기울이면.gif","LOVE.gif","LP.gif","man.gif","SIGA2
 
 const chosenImage = login_image[Math.floor(Math.random() * login_image.length)];
 const mainselect=document.getElementById("logo_image")
+
+
 console.log(mainselect);
+
+
 const loginimg = document.createElement("img");
 loginimg.src=`loginimg/${chosenImage}`;
 
